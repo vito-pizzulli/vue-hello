48c20c1c79a02622af6e8e48605fbfbcd728a44b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-01 14:42:42 +0200

    bonus completed, img printed from data

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            image: 'img/vue.gif'
         }
     }
 }) .mount('#app');
